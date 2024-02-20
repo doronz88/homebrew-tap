0a9f518859d657712dada219e9f8c35f4e97b736
GitHub Action <action@github.com>
2024-02-20 23:25:38 +0000

    Update pymobiledevice3 formula to version

--- Formula/pymobiledevice3.rb
+++ Formula/pymobiledevice3.rb
@@ -3,7 +3,7 @@ class Pymobiledevice3 < Formula
   
     desc "Pure python3 implementation for working with iDevices (iPhone, etc...)."
     homepage "https://github.com/doronz88/pymobiledevice3"
-    url "https://files.pythonhosted.org/packages/39/a4/240973e04f9a39800d6286bdc69f6ede71832d3fe62cc8a2d95b1ba8dbec/pymobiledevice3-2.43.5.tar.gz"
+    url "https://files.pythonhosted.org/packages/98/a6/31f16e9a94d5d9e34995ef28bbab11de0cd43fa7b54f72358f92a34e72eb/pymobiledevice3-2.43.5-py3-none-any.whl"
     sha256 "70c59d8ed036d360ae05affbff51679385761396775cbfdf4e2559071c1b7e5e"
 
     depends_on "python@3.11"
